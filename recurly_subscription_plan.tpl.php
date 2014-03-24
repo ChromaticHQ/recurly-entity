0913c7bc60bd5239bdadc745f8147510a6a52a03
Andrew Berry <andrew.berry@lullabot.com>
2014-03-24 17:02:00 -0400

    GCDEV-50: View all properties of a subscription plan.

---
+++ recurly_subscription_plan.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Default template for Recurly Subscription Plan entities.
+ *
+ * TODO: Document all of the keys in $content. For now, reference
+ * RecurlySubscriptionPlanMetadataController.
+ */
+?>
+<?php print render($content); ?>
+
